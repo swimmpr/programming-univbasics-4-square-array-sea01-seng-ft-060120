0e55d76c9f1ca69f60558a8365f6e5874ad2464f
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-12 17:24:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
-def square_array(array)
+def square_array(numbers)
   new_array = []
-
- array.each do |integer|
-  new_array << integer ** 2
- end
- return new_array
+  counter = 0 
+  while counter < numbers.length()
+  new_array.push(numbers[counter] * numbers[counter])
+  counter += 1
+  end 
+  return new_array
 end
